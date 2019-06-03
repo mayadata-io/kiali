6c444d7900b39154af754ffb73cd462ee99df7b3
inyee786 <intakhab.ali@mayadata.io>
2019-06-03 17:49:53 +0530

    update travis with docker file

--- build-script.sh
+++ build-script.sh
@@ -8,9 +8,6 @@ cd ../
 git clone https://github.com/mayadata-io/kiali-ui.git
 cd kiali-ui/
 NODE_OPTIONS=--max_old_space_size=4096
-git checkout master
-npm install -g yarn
-yarn --frozen-lockfile --non-interactive || (echo 'package.json is not in sync with yarn.lock, check that you include yarn.lock' && false)
-yarn prettier --list-different
-yarn build
+git checkout mayadata-io
+docker build mayadataio/kiali-ui:${COMMIT} .
 # cd ../kiali
