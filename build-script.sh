422ae221f15423e3f727dc639ecd99bace434f90
inyee786 <intakhab.ali@mayadata.io>
2019-06-03 12:46:09 +0530

    update travis file for nodejs dir

--- build-script.sh
+++ build-script.sh
@@ -4,8 +4,10 @@ echo $KIALI_PATH
 echo $GOPATH
 echo $GO_BUILD_ENVVARS
 # build kiali-ui
+cd ../
 git clone https://github.com/mayadata-io/kiali-ui.git
-git checout master
+git checkout master
 npm install -g yarn
 yarn
 yarn build
+cd ../kiali
