fa50126f1903102213b29c2d26db53508c4d8cca
inyee786 <intakhab.ali@mayadata.io>
2019-06-03 14:28:10 +0530

    update the travis with old

--- build-script.sh
+++ build-script.sh
@@ -7,9 +7,10 @@ echo $GO_BUILD_ENVVARS
 cd ../
 git clone https://github.com/mayadata-io/kiali-ui.git
 cd kiali-ui/
+NODE_OPTIONS=--max_old_space_size=4096
 git checkout master
 npm install -g yarn
 yarn --frozen-lockfile --non-interactive || (echo 'package.json is not in sync with yarn.lock, check that you include yarn.lock' && false)
-# yarn prettier --list-different
-# yarn build:dev
+yarn prettier --list-different
+yarn build
 # cd ../kiali
