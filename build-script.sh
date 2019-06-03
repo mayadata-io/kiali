d93b3c811221a6dd4718ca3c364c223e255a0449
inyee786 <intakhab.ali@mayadata.io>
2019-06-03 14:07:17 +0530

    update travis

--- build-script.sh
+++ build-script.sh
@@ -8,8 +8,8 @@ cd ../
 git clone https://github.com/mayadata-io/kiali-ui.git
 cd kiali-ui/
 git checkout master
-# npm install -g yarn
-# yarn --frozen-lockfile --non-interactive || (echo 'package.json is not in sync with yarn.lock, check that you include yarn.lock' && false)
+npm install -g yarn
+yarn --frozen-lockfile --non-interactive || (echo 'package.json is not in sync with yarn.lock, check that you include yarn.lock' && false)
 # yarn prettier --list-different
 # yarn build:dev
 # cd ../kiali
